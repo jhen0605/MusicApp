ff7d17b51e9aabc3c03a321e75aaf8ef717824df
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-04-28 00:32:37 +0800

    Initial Commit

---
+++ Music App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music App
+//
+//  Created by 簡吟真 on 2021/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
